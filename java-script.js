309e83fd6cabd7f23c4742972619d68e80d0c5b2
Yurii Pochapskiy <pochapskiy1983@gmail.com>
2025-10-19 22:03:44 +0300

    add: closr menu if click to item

--- java-script.js
+++ java-script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const openButtonMenu = document.getElementById("openButtonMenu");
     const closeButtonMenu = document.getElementById("closeButtonMenu");
     const overLayMenu = document.getElementById("overLayMenu");
+    const menuLinks = overLayMenu.querySelectorAll("a.nav_header_link")
 
     openButtonMenu?.addEventListener("click", function () {
         overLayMenu.classList.add("is-open")
@@ -9,5 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     closeButtonMenu?.addEventListener("click", function () {
         overLayMenu.classList.remove("is-open")
+    });
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            overLayMenu.classList.remove("is-open");
+        });
     });
 });
